a4d9e284b23f1e3a48185d49e607c4dea91b3a89
Korwith <80077493+Korwith@users.noreply.github.com>
2024-09-23 12:31:35 -0400

    added tab select

--- v2/view.js
+++ v2/view.js
@@ -2,8 +2,10 @@ const page_button = document.querySelectorAll('.page_button');
 const tracker_page = document.querySelector('.page.tracker');
 const logo_button = document.querySelector('.icon.logo');
 
+const horizontal = document.querySelector('.horizontal');
 const sidebar = document.querySelector('nav.sidebar');
 const content = document.querySelector('.content');
+const tab_select = document.querySelector('.tab_select');
 
 const iframe_link = {
     'banner': 'https://www.tradingview-widget.com/embed-widget/ticker-tape/?locale=en#%7B%22symbols%22%3A%5B%7B%22proName%22%3A%22FOREXCOM%3ASPXUSD%22%2C%22title%22%3A%22S%26P%20500%20Index%22%7D%2C%7B%22proName%22%3A%22FOREXCOM%3ANSXUSD%22%2C%22title%22%3A%22US%20100%20Cash%20CFD%22%7D%2C%7B%22proName%22%3A%22FX_IDC%3AEURUSD%22%2C%22title%22%3A%22EUR%20to%20USD%22%7D%2C%7B%22proName%22%3A%22BITSTAMP%3ABTCUSD%22%2C%22title%22%3A%22Bitcoin%22%7D%2C%7B%22proName%22%3A%22BITSTAMP%3AETHUSD%22%2C%22title%22%3A%22Ethereum%22%7D%5D%2C%22showSymbolLogo%22%3Atrue%2C%22isTransparent%22%3Atrue%2C%22displayMode%22%3A%22compact%22%2C%22colorTheme%22%3A%22dark%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A104%2C%22utm_source%22%3A%22www.tradingview.com%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22ticker-tape%22%2C%22page-uri%22%3A%22www.tradingview.com%2Fwidget-wizard%2Fen%2Flight%2Fticker-tape%2F%22%7D',
@@ -63,6 +65,30 @@ function loadHomeWidgets() {
     }
 }
 
+function openTab(event) {
+    if (!event.target) { return false };
+    let to_open = event.target.getAttribute('open');
+    if (!to_open) { return false };
+    if (event.target.classList.contains('active')) { return false };
+    
+    let active_tab = tab_select.querySelector('.active');
+    active_tab.classList.remove('active');
+    event.target.classList.add('active');
+
+    let active_page = horizontal.querySelector('.show');
+    let to_open_dom = horizontal.querySelector('.' + to_open);
+    to_open_dom.classList.add('show');
+    active_page.classList.remove('show');
+}
+
+function handleTabSelect() {
+    let tab_button = tab_select.querySelectorAll('button');
+    for (var i = 0; i < tab_button.length; i++) {
+        let this_button = tab_button[i];
+        this_button.onclick = openTab;
+    }
+}
+
 // Handle tacker page
 function loadTrackerWidget() {
     let widget = new TradingView.widget({
@@ -96,4 +122,5 @@ function loadTrackerWidget() {
 handlePageButton();
 loadHomeWidgets();
 loadTrackerWidget();
+handleTabSelect();
 logo_button.onclick = sidebarButton;
